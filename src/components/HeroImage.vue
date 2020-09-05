365164209705d39e4e7b4e42ad608801eebde085
hajzeer <krzysh98@gmail.com>
2020-09-05 17:11:28 +0200

    fix heroImage

--- src/components/HeroImage.vue
+++ src/components/HeroImage.vue
@@ -54,17 +54,6 @@ name: "HeroImage"
     background-position: 50% 50%;
     background-repeat: no-repeat;
     background-size: cover;
-
-    &::after {
-      content: '';
-      background: rgba(0, 0, 0, .2);
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      z-index: 1;
-      width: 100%;
-      height: 100vh;
-    }
   }
 }
 </style>
